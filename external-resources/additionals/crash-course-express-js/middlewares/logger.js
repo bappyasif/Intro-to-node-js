ebd2181d0b2315adc69cfc8e4c6d37bcc1fa620e
bappyasif <bappyasif@hotmail.com>
2022-08-11 22:44:37 +0600

    cleaning it up for heroku app

--- external-resources/additionals/crash-course-express-js/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-let moment = require("moment")
-
-let logger = (req, res, next) => {
-    console.log('logging middleware', `${req.protocol}://${req.get("host")}${req.originalUrl} : ${moment().format()}`)
-    next()
-}
-
-module.exports = logger
