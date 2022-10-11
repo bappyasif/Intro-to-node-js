2f3771700dc3e24ff60afc066d67db5c6dcf7b79
bappyasif <bappyasif@hotmail.com>
2022-10-11 22:31:07 +0600

    next and previous functionlaity is added
    * blog sites mark is adjusted
    * admin site styles been adjusted

--- client-sides/public/index.js
+++ client-sides/public/index.js
@@ -1,16 +1,66 @@
+let blogsData = []
+let postsContainer = document.querySelector(".blogs")
+let CHUNK = 1;
+let CURRENT = 0;
+
 let fetchAllBlogPosts = () => {
     fetch("http://localhost:3000/blog/all-posts/")
         .then(resp => resp.json())
         .catch(err => console.error('Caught error: ', err))
-            .then(data => renderData(data.posts))
-            .catch(err => new Error("error occured", err))
+        .then(data => {
+            renderDataByChunk(data.posts, CHUNK, CURRENT);
+            blogsData = data.posts.filter(item => item.published);
+            CURRENT = CURRENT + CHUNK;
+            // console.log(blogsData, "blogsData")
+        })
+        .catch(err => new Error("error occured", err))
 }
 
-let renderData = (data) => {
-    let postsContainer = document.querySelector(".container")
-    console.log(data, "!!")
-    data.forEach(postObj => {
-        if(postObj.published) {
+let handlePrevious = () => {
+    // console.log("before prev", CURRENT)
+    if (CURRENT > 0) {
+        let temp = CURRENT - CHUNK
+
+        cleanExistingBlogPosts();
+
+        renderDataByChunk(blogsData, CURRENT, temp)
+        CURRENT = temp;
+    }
+    // console.log("after prev", CURRENT)
+
+    // Keeping CURRENT at 1 as a starting point for next cycle operations
+    if (CURRENT == 0) {
+        CURRENT = 1
+    }
+}
+
+let handleNext = () => {
+    // console.log("before next", CURRENT)
+    if (CURRENT <= blogsData.length - 1) {
+        let temp = CURRENT + CHUNK
+
+        cleanExistingBlogPosts();
+
+        renderDataByChunk(blogsData, temp, CURRENT)
+        CURRENT = temp;
+    }
+    // console.log("after next", CURRENT)
+    
+    // keeping CURRENT at previous point so that "previous" cycle doesnt have to wait a click
+    if (CURRENT == blogsData.length) {
+        CURRENT = CURRENT - CHUNK
+    }
+}
+
+let cleanExistingBlogPosts = () => {
+    postsContainer.childNodes.forEach(node => {
+        node.textContent = ''
+    })
+}
+
+let renderDataByChunk = (data, limit, starting) => {
+    data.forEach((postObj, idx) => {
+        if (idx >= starting && idx < limit) {
             postMarkup(postObj, postsContainer)
         }
     })
@@ -40,5 +90,125 @@ let createMarkup = (data, type, className) => {
     return element
 }
 
+// next button event listener
+let nextBtn = document.querySelector(".next-btn");
+nextBtn.addEventListener("click", handleNext)
+
+// previous button event listener
+let prevBtn = document.querySelector(".prev-btn");
+prevBtn.addEventListener("click", handlePrevious)
+
 // begin fetching all posts
 fetchAllBlogPosts();
+
+/**
+ * 
+ * 
+ let renderDataByChunk = (data, limit, starting) => {
+    data.forEach((postObj, idx) => {
+        if (postObj.published) {
+            if (idx >= starting && idx < limit) {
+                postMarkup(postObj, postsContainer)
+            }
+        }
+        // if(postObj.published && idx >= CURRENT && idx < limitBy) {
+        //     postMarkup(postObj, postsContainer)
+        // }
+    })
+}
+ * 
+ * 
+ // let handlePrevious = () => {
+//     let temp = CURRENT - CHUNK;
+
+//     if(temp >= 0) {
+//         if (blogsData[temp].published) {
+//             cleanExistingBlogPosts();
+//             renderDataByChunk(blogsData, CURRENT, temp)        
+//         } else {
+//             temp = CURRENT - CHUNK;
+//         }
+//     }
+
+//     CURRENT = temp;
+    
+//     console.log(temp,"<><>", CURRENT, blogsData[temp], "[][]", blogsData[CURRENT])
+    
+//     // if (temp >= 0 && CURRENT <= blogsData.length - 1) {
+//     //     renderDataByChunk(blogsData, CURRENT, temp)
+//     //     CURRENT = temp;
+//     // }
+// }
+ * 
+ * 
+ // let handlePrevious = () => {
+//     let temp = CURRENT - CHUNK;
+
+//     if (blogsData[temp].published) {
+//         cleanExistingBlogPosts();
+//     }
+    
+//     console.log(temp,"<><>", CURRENT, blogsData[temp], "[][]", blogsData[CURRENT])
+    
+//     if (temp >= 0 && CURRENT <= blogsData.length - 1) {
+//         renderDataByChunk(blogsData, CURRENT, temp)
+//         CURRENT = temp;
+//     }
+// }
+ * 
+ * 
+ 
+// let handlePrevious = () => {
+//     let temp = CURRENT - CHUNK
+//     if (blogsData[temp].published) {
+//         cleanExistingBlogPosts();
+//     }
+    
+//     console.log(temp,"<><>", CURRENT)
+    
+//     if (temp >= 0 && CURRENT <= blogsData.length - 1) {
+
+//         renderDataByChunk(blogsData, CURRENT, temp)
+//         CURRENT = temp;
+//     }
+// }
+ * 
+ * 
+ 
+// let initialRender = (data) => {
+//     // let postsContainer = document.querySelector(".blogs")
+//     console.log(data, "!!")
+//     // data.forEach((postObj, idx) => {
+//     //     if(postObj.published && idx < 1) {
+//     //         postMarkup(postObj, postsContainer)
+//     //     }
+//     // })
+//     renderDataByChunk(data, 1)
+// }
+
+// let handlePrevious = () => {
+//     if(CURRENT > 0 && CURRENT <= blogsData.length - 1) {
+//         console.log(CURRENT, "<1><1>", CHUNK)
+//         let temp = CURRENT - CHUNK
+
+//         if(blogsData[temp].published) {
+//             cleanExistingBlogPosts();
+//         }
+
+//         renderDataByChunk(blogsData, CURRENT);
+//         CURRENT = temp;
+//         console.log(CURRENT, "<2><2>", CHUNK)
+//     }
+// }
+
+// let handlePrevious = () => {
+//     if(CURRENT > 0 && CURRENT <= blogsData.length - 1) {
+//         let temp = CURRENT - CHUNK
+//         if(blogsData[temp].published) {
+//             cleanExistingBlogPosts();
+//         }
+//         renderDataByChunk(blogsData, CURRENT, temp)
+//         CURRENT = temp;
+//     }
+// }
+ */
