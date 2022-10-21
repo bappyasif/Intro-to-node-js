92d56ecb34bfaab97a335d8420117845f622ab99
bappyasif <bappyasif@hotmail.com>
2022-10-22 00:38:28 +0600

    manin navigation view implemented
    * used mui
    * some basic flex styling

--- client/src/components/MainNavigation.js
+++ client/src/components/MainNavigation.js
@@ -1,15 +1,73 @@
-import React from 'react'
-import { H1Element, LinkElement, NavElement, WrapperDiv } from './GeneralElements'
+import React, { useEffect, useState } from 'react'
+import { LoginTwoTone, AppRegistrationTwoTone, TimelineTwoTone } from "@mui/icons-material";
+import { H1Element, H4Element, WrapperDiv } from './GeneralElements'
+import { MuiButtonElement, MuiInputElement, TabElement, TabsElement } from './MuiElements';
+import { FormElement } from './FormElements';
+import { sendDataToServer } from './utils';
+import { AppContexts } from '../App';
 
 function MainNavigation() {
+  const [value, setValue] = React.useState(9);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <WrapperDiv className="nav-wrapper">
       <H1Element value={"Main Navigation"} />
-      <NavElement>
+      <TabsElement className="main-nav" handleChange={handleChange} value={-1}>
+        <TabElement className={"nav-item"} labelText={"Login"} path={"login"} icon={<LoginTwoTone />} />
+        <TabElement className={"nav-item"} labelText={"Register"} path={"register"} icon={<AppRegistrationTwoTone />} />
+        <TabElement className={"nav-item"} labelText={"Timeline"} path={"timeline"} icon={<TimelineTwoTone />} />
+      </TabsElement>
+      {/* <NavElement>
         <LinkElement className={"nav-item"} value={"Login"} path={"login"} />
         <LinkElement className={"nav-item"} value={"Register"} path={"register"} />
         <LinkElement className={"nav-item"} value={"Timeline"} path={"timeline"} />
-      </NavElement>
+      </NavElement> */}
+      <FloatingLogin />
+    </WrapperDiv>
+  )
+}
+
+let FloatingLogin = () => {
+  let [errors, setErrors] = useState([]);
+  let [formData, setFormData] = useState({});
+  const enpoint = React.useContext(AppContexts)
+
+  let handleChange = (evt, elm) => setFormData(prev => ({ ...prev, [elm]: evt.target.value }))
+
+  let handleError = data => setErrors(data.errors)
+
+  let handleSubmit = evt => {
+    evt.preventDefault();
+    sendDataToServer(enpoint.baseUrl + "login", formData, handleError)
+  }
+  useEffect(() => handleError([]), [formData])
+  return (
+    <WrapperDiv className="fl-wrapper">
+      <H4Element value={"Login to your profile"} />
+      <FormElement handleSubmit={handleSubmit}>
+        <MuiInputElement
+          type={"email"}
+          id={"email"}
+          handleChange={handleChange}
+          text="enter email (e.g. t@e.st)"
+          required={true}
+          color={errors?.length ? "error" : "success"}
+          error={errors?.length ? true : false}
+        />
+        <MuiInputElement
+          type={"password"}
+          id={"password"}
+          handleChange={handleChange}
+          text="enter password"
+          required={true}
+          color={errors?.length ? "error" : "success"}
+          error={errors?.length ? true : false}
+        />
+        <MuiButtonElement type={"submit"} text="Login" />
+      </FormElement>
     </WrapperDiv>
   )
 }
---
+++ client/src/components/MuiElements/index.js
@@ -0,0 +1,74 @@
+import { Tab, Tabs, Input, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const TabsElement = (props) => {
+    return (
+        <Tabs
+            value={props.value}
+            onChange={props.handleChange}
+            className={props.className}
+            centered
+        >{props.children}</Tabs>
+    )
+}
+
+const TabElement = (props) => {
+    return (
+        <Tab
+            icon={props.icon}
+            label={props.labelText}
+            to={props.path}
+            LinkComponent={Link}
+        >
+            {props.children}
+        </Tab>
+    )
+}
+
+const TabElementWithoutLink = ({ icon, labelText }) => {
+    return <Tab icon={icon} label={labelText} />
+}
+
+const MuiInputElement = ({ id, type, handleChange, text, required, color, error }) => {
+    console.log(error)
+    return (
+        <Input 
+            id={id}
+            name={id}
+            type={type}
+            onChange={handleChange}
+            placeholder={text}
+            required={required}
+            color={color}
+            // fullWidth={true}
+            error={error}
+        />
+    )
+}
+
+const MuiButtonElement = ({type, text}) =>{
+    return (
+        <Button variant="contained" color={"success"} type={type}>{text}</Button>
+    )
+}
+
+const MuiBoxElement = (props) => {
+    return (
+        <Box
+            sx={{border: 1.1, borderColor: props.color}} 
+            color={props.color}>{props.children}</Box>
+    )
+}
+
+// const MuiIconElement = ({icon}) => {
+    
+// }
+
+export {
+    TabsElement,
+    TabElement,
+    TabElementWithoutLink,
+    MuiInputElement,
+    MuiButtonElement,
+    MuiBoxElement
+}
