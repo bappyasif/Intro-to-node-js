fd1519966698564ac4e771af9a57c0fb00156573
bappyasif <bappyasif@hotmail.com>
2022-11-12 22:59:01 +0600

    added tweet embed view
    * also cleaned up code from earlier

--- client/src/components/routes/UserSpecificNewsFeeds.js
+++ client/src/components/routes/UserSpecificNewsFeeds.js
@@ -7,33 +7,8 @@ import { readDataFromServer } from '../utils';
 
 function UserSpecificNewsFeeds() {
     let [postsDataset, setPostsDataset] = useState([]);
-    let [filteredDataset, setFilteredDataset] = useState([]);
-    let [decidedDataset, setDecidedDataset] = useState([]);
 
     let appCtx = useContext(AppContexts);
-
-    // const getDuplicates = (arr, key) => {
-    //     let filteredIds = arr.map(item => item.postData.id)
-    //         .filter((value, index, self) => self.indexOf(value) !== index)
-
-    //     let filtered = [];
-
-    //     postsDataset.forEach(item => {
-    //         let chk = filtered.findIndex(elem => elem.postData.id === item.postData.id)
-    //         if (filteredIds.includes(item.postData.id) && chk === -1) {
-    //             console.log(item, item.postData.id)
-    //             filtered.push(item)
-    //         }
-    //     })
-    //     // console.log(filtered, postsDataset, filteredIds)
-    //     setFilteredDataset(filtered)
-    // }
-
-    // let handleDataset = result => {
-    //     console.log(result, "result!!", ...result.data?.data, "<><>", postsDataset)
-
-    //     result?.data?.data && setPostsDataset(prev => ([...prev, ...result.data.data]))
-    // }
 
     let handleDataset = result => {
         console.log(result, "result!!", ...result.data?.data, "<><>", postsDataset)
@@ -58,31 +33,26 @@ function UserSpecificNewsFeeds() {
         }
     }, [topics])
 
-    // useEffect(() => getDuplicates(postsDataset), [postsDataset])
-
-    // useEffect(() => filteredDataset?.length && setPostsDataset(filteredDataset), [filteredDataset])
-
-    // console.log(postsDataset, "postsDataset!!", filteredDataset)
     console.log(postsDataset, "postsDataset!!")
 
-    // useEffect(() => setDecidedDataset(filteredDataset.length ? filteredDataset : postsDataset), [postsDataset, filteredDataset])
-
-    // let renderPosts = () => (decidedDataset)?.map(dataset => <RenderPost key={dataset?.postData.id} item={dataset} baseUrl={appCtx.baseUrl} />)
     let renderPosts = () => postsDataset?.map(dataset => <RenderPost key={dataset?.postData.id} item={dataset} baseUrl={appCtx.baseUrl} />)
 
     return (
         <Paper>
             <Typography variant='h1'>User Specific News Feeds</Typography>
-            {/* <TweetEmbed tweetsDataset={postsDataset} /> */}
+            <TweetEmbed tweetsDataset={postsDataset} />
             {renderPosts()}
         </Paper>
     )
 }
 
 const TweetEmbed = ({ tweetsDataset }) => {
-    let renderEmbeds = () => tweetsDataset?.map(tweetDataset => <TwitterTweetEmbed key={tweetDataset.postData.id} />)
+    let renderEmbeds = () => tweetsDataset?.map(tweetDataset => <TwitterTweetEmbed key={tweetDataset.postData.id} tweetId={tweetDataset.postData.id} onLoad={function noRefCheck(){}} placeholder="Loading" />)
     return (
-        <Paper>
+        <Paper
+            className='embeds-wrap'
+            sx={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}
+        >
             {renderEmbeds()}
         </Paper>
     )
