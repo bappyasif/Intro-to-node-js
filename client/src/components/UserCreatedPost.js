5e041ac9835271571dcc258e2f59f1c315516a22
bappyasif <bappyasif@hotmail.com>
2022-11-20 22:34:03 +0600

    count data update is now functioning without error
    * user specific count is getting added to "counts" so that server can handle data properly
    * a user specific count is sending out to server for keeping track of in its "engggedUsers" property

--- client/src/components/UserCreatedPost.js
+++ client/src/components/UserCreatedPost.js
@@ -53,6 +53,7 @@ function ShowUserCreatedPost({ postData }) {
 
 let UserEngagementWithPost = ({ postData, appCtx }) => {
   let [counts, setCounts] = useState({})
+  let [onlyUserCounts, setOnlyUserCounts] = useState({})
   let [time, setTime] = useState(null);
   let [session, setSession] = useState(null);
   let [dataReady, setDataReady] = useState(false)
@@ -61,23 +62,26 @@ let UserEngagementWithPost = ({ postData, appCtx }) => {
     // setCounts(prev => ({...prev, [elem]: prev[elem] ? prev[elem] + 1 : 1}))
     // setCounts(prev => ({...prev, [elem]: (prev[elem] && addFlag) ? prev[elem] + 1 : prev[elem] - 1}))
     setCounts(prev => ({ ...prev, [elem]: (prev[elem] >= 0 && addFlag) ? prev[elem] + 1 : prev[elem] - 1 }))
+    // setCounts(prev => ({ ...prev, [elem]: (prev[elem] >= 0 && addFlag) ? prev[elem] + 1 : prev[elem] - 1 }))
+    setOnlyUserCounts(prev => ({...prev, [elem]: prev[elem] ? 0 : 1}))
 
     // clearing out previously existing timeout element
-    clearTimeout(session);
+    session && clearTimeout(session);
     // clearing out previously existing session element
-    setSession(null)
+    // setSession(null)
     // setting a new timer with 2000ms, so that timer can take effect after that time
     setTime(2000);
     // calling up timer to kick start timer function
     // timer(); // timer isn't working properly!!
-    updateThisPostCountsInDatabase() // without it data update runs just fine
+    // updateThisPostCountsInDatabase() // without it data update runs just fine
   }
 
   let timer = () => {
     console.log("begin timer")
     let sesn = setTimeout(() => {
       // this gets to run only when user is not interacting
-      if (time === 2000) {
+      console.log("running!!", time)
+      if (time >= 2000) {
         setDataReady(true);
         console.log("!!")
         clearTimeout(sesn);
@@ -91,10 +95,16 @@ let UserEngagementWithPost = ({ postData, appCtx }) => {
     console.log("begin session")
   }
 
+  useEffect(() => {
+    time && timer()
+  }, [time])
+
   let updateThisPostCountsInDatabase = () => {
     // let url = `${appCtx.baseUrl}/posts/${postData._id}`
     let url = `${appCtx.baseUrl}/posts/${postData._id}/${appCtx.user._id}`
-    console.log(url, "url!!", counts)
+    
+    counts.currentUserCounts = onlyUserCounts;
+    console.log(url, "url!!", counts, onlyUserCounts)
 
     updateDataInDatabase(url, counts)
   }
@@ -116,6 +126,9 @@ let UserEngagementWithPost = ({ postData, appCtx }) => {
       Share: postData?.shareCount || 0,
       // engaggedUser: Object.values(postData?.usersEngagged[findIdx])[0] || []
     })
+
+    // initializing user specific counts
+    setOnlyUserCounts({Like: 0, Love: 0, Dislike: 0, Share: 0})
   }, [])
 
   useEffect(() => {
@@ -126,6 +139,8 @@ let UserEngagementWithPost = ({ postData, appCtx }) => {
       setCounts(prev => ({...prev, engaggedUser: Object.values(postData?.usersEngagged[findIdx])[0]}))
     }
   }, [postData])
+
+  console.log(session, "session!!", dataReady, counts, time)
 
   return (
     <Stack
@@ -145,6 +160,7 @@ let RenderActionableIcon = ({ item, handleCounts, counts }) => {
 
   let handleClick = () => {
     setFlag(!flag);
+    console.log(flag, "flag", !flag, !!flag)
     handleCounts(item.name, !flag);
   }
 
@@ -152,6 +168,7 @@ let RenderActionableIcon = ({ item, handleCounts, counts }) => {
   useEffect(() => {
     // toggling through +1 or -1 value for specefic count, based on flag current value for that item
     // initFlag && handleCounts(item.name, flag);
+    // handleCounts(item.name, flag);
   }, [flag])
 
   // useEffect(() => setInitFlag(true), [])
@@ -165,7 +182,7 @@ let RenderActionableIcon = ({ item, handleCounts, counts }) => {
   // }, [counts])
 
   // console.log(counts?.engaggedUser, counts?.engaggedUser[item.name])
-  console.log(flag, "flag outside")
+  // console.log(flag, "flag outside")
 
   return (
     <Tooltip title={(flag) ? `${item.name}ed already` : item.name}>
--- controllers/post.js
+++ controllers/post.js
@@ -113,47 +113,73 @@ const updateSoloPost = (req, res, next) => {
 
 const updateSoloPostWithUserEngagements = (req, res, next) => {
     let data = req.body;
-    console.log(data, "!!", req.params.postId, req.params.interactingUserId)
+    console.log(data, "!!", req.params.postId, req.params.interactingUserId, data.currentUserCounts)
 
     Post.findOne({ _id: req.params.postId })
         .then(currentPost => {
             // updating post with data sent to server from client
-            currentPost.likesCount = 0
-                // (currentPost.likesCount && data.Like)
-                //     ? currentPost.likesCount + 1
-                //     : (!currentPost.likesCount && data.Like)
-                //         ? data.Like
-                //         : 1;
+            // console.log(data.Like ? currentPost.likesCount + 1 : 1, data.Like, currentPost.likesCount + 1 )
+            // currentPost.likesCount = data.Like ? currentPost.likesCount + 1 : 1;
+            currentPost.likesCount = data.Like;
+            // currentPost.likesCount = 0;
 
-            currentPost.dislikesCount = 0
-                // (currentPost.dislikesCount && data.Dislike)
-                //     ? currentPost.dislikesCount + 1
-                //     : (!currentPost.dislikesCount && data.Dislike)
-                //         ? data.Dislike
-                //         : 1;
+            // (currentPost.likesCount && data.Like)
+            //     ? currentPost.likesCount + 1
+            // : (!currentPost.likesCount && data.Like)
+            // data.Like
+            //     ? currentPost.likesCount + 1
+            //     : 1;
 
-            currentPost.loveCount = 0
-                // (currentPost.loveCount && data.Love)
-                //     ? currentPost.loveCount + 1
-                //     : (!currentPost.loveCount && data.Love)
-                //         ? data.Love
-                //         : 1;
+            // currentPost.dislikesCount = 0;
+            currentPost.dislikesCount = data.Dislike;
+            // currentPost.dislikesCount = data.Dislike ? currentPost.dislikesCount + 1 : 1;
+            // console.log(data.Dislike ? currentPost.dislikesCount + 1 : 1, data.Dislike, currentPost.dislikesCount + 1)
 
-            currentPost.shareCount = 0
-                // (currentPost.shareCount && data.Share)
-                //     ? currentPost.shareCount + 1
-                //     : (!currentPost.shareCount && data.Share)
-                //         ? data.Share
-                //         : 1
+            // (currentPost.dislikesCount && data.Dislike)
+            //     ? currentPost.dislikesCount + 1
+            // : (!currentPost.dislikesCount && data.Dislike)
+            // data.Dislike
+            //     ? currentPost.dislikesCount + 1
+            //     : 1;
+
+            // currentPost.loveCount = 0
+            currentPost.loveCount = data.Love
+            // currentPost.loveCount = data.Love ? currentPost.loveCount + 1 : 1;
+            // console.log(data.Love ? currentPost.loveCount + 1 : 1, data.Love, currentPost.loveCount + 1)
+
+            // (currentPost.loveCount && data.Love)
+            //     ? currentPost.loveCount + 1
+            // : (!currentPost.loveCount && data.Love)
+            // data.Love
+            //     ? currentPost.loveCount + 1
+            //     : 1;
+
+            // currentPost.shareCount = 0
+            currentPost.shareCount = data.Share
+            // currentPost.shareCount = data.Share ? currentPost.shareCount + 1 : 1
+            // console.log(data.Share ? currentPost.shareCount + 1 : 1, data.Share , currentPost.shareCount + 1)
+
+            // (currentPost.shareCount && data.Share)
+            //     ? currentPost.shareCount + 1
+            // : (!currentPost.shareCount && data.Share)
+            //     ? data.Share
+            // data.share
+            //     ? currentPost.shareCount + 1
+            //     : 1
 
             let findIdx = currentPost.usersEngagged?.findIndex(item => Object.keys(item)[0] === req.params.interactingUserId)
-            
-            if(findIdx === -1) {
-                currentPost.usersEngagged.push({ [req.params.interactingUserId]: { Like: 0, Love: 0, Dislikes: 0, Share: 0 } })
+
+            if (findIdx === -1) {
+                console.log("check notfound!!")
+                currentPost.usersEngagged.push({ [req.params.interactingUserId]: data.currentUserCounts })
+                // currentPost.usersEngagged.push({ [req.params.interactingUserId]: { Like: 0, Love: 0, Dislikes: 0, Share: 0 } })
                 // currentPost.usersEngagged.push({ [req.params.interactingUserId]: { Like: data.Like, Love: data.Love, Dislikes: data.Dislike, Share: data.Share } })
             } else {
+                console.log("check found!!")
                 // currentPost.usersEngagged[findIdx] = { [req.params.interactingUserId]: { Like: data.Like, Love: data.Love, Dislikes: data.Dislike, Share: data.Share } }
-                currentPost.usersEngagged[findIdx] = { [req.params.interactingUserId]: { Like: 0, Love: 0, Dislikes: 0, Share: 0 } }
+                // currentPost.usersEngagged[findIdx] = { [req.params.interactingUserId]: { Like: 0, Love: 0, Dislikes: 0, Share: 0 } }
+                
+                currentPost.usersEngagged[findIdx] = { [req.params.interactingUserId]: data.currentUserCounts }
             }
 
             console.log(currentPost, "currentPost!!")
