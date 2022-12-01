d7225626f580de14b4dfed037562d8b5e85816d5
bappyasif <bappyasif@hotmail.com>
2022-12-01 23:16:34 +0600

    profile edit revamped ui in place
    * profile edit renders both picture elemnts from same reusable component
    * form data is rendered after filtering sensitive informations
    * form data is rendered from a resuable component rather than individual implementations of them
    * form has now options to either save or cancel any changes they might have had

--- client/src/components/routes/EditUserProfile.js
+++ client/src/components/routes/EditUserProfile.js
@@ -1,22 +1,56 @@
-import { WallpaperRounded } from '@mui/icons-material'
-import { Box, FormControl, IconButton, ImageListItem, ImageListItemBar, Input, InputLabel, Stack, TextareaAutosize, Typography } from '@mui/material'
+import { NotInterestedTwoTone, SaveAltTwoTone, WallpaperRounded } from '@mui/icons-material'
+import { Box, Button, FormControl, IconButton, ImageListItem, ImageListItemBar, Input, InputLabel, Paper, Stack, TextareaAutosize, Typography } from '@mui/material'
 import moment from 'moment'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContexts } from '../../App'
 import { fakeDataModel } from '../UserProfileInfoSection'
 
 function EditUserProfile() {
     let [userData, setUserData] = useState({})
 
+    let appCtx = useContext(AppContexts);
+
     let handleData = (evt, elem) => setUserData(prev => ({ ...prev, [elem]: evt.target.value }))
 
-    useEffect(() => setUserData(fakeDataModel[0]), [])
+    useEffect(() => setUserData(appCtx.user || fakeDataModel[0]), [])
+
+    // console.log(userData, "!!")
 
     return (
         <Box>
             <Typography variant='h1'>Edit User Profile</Typography>
-            <CoverPhoto />
-            {userData.fullName ? <RenderFormWithData handleData={handleData} data={userData} /> : null}
+            {/* <CoverPhoto /> */}
+            {userData.created ? <RenderPhoto cpUrl={userData.cpUrl} fullName={userData.fullName} /> : null}
+            {userData.created ? <RenderFormWithData handleData={handleData} data={userData} /> : null}
+            {userData.created ? <RenderFormActionButtons userData={userData} /> : null }
         </Box>
+    )
+}
+
+let RenderFormActionButtons = ({userData}) => {
+    let buttons = [{name: "Save", icon: <SaveAltTwoTone />}, {name: "Cancel", icon: <NotInterestedTwoTone />}]
+    
+    let renderButtons = () => buttons.map(item => <RenderActionButton key={item.name} item={item} userData={userData} />)
+    
+    return (
+        <Stack sx={{flexDirection: "row", gap: 4, justifyContent: "center"}}>
+            {renderButtons()}
+        </Stack>
+    )
+}
+
+let RenderActionButton = ({item, userData}) => {
+    let handleClick = () => {
+        if(item.name === "Save") {
+            console.log(userData, "save!!")
+        } else {
+           alert("goto profile!!") 
+        }
+    }
+    return (
+        <Button onClick={handleClick} size='large' startIcon={item.icon} variant="outlined" sx={{verticalAlign: "middle"}}>
+            <Typography variant='h6' fontWeight={"bold"}>{item.name}</Typography>
+        </Button>
     )
 }
 
@@ -24,15 +58,29 @@ let RenderFormWithData = ({ handleData, data }) => {
     let renderData = []
 
     for (let key in data) {
-        renderData.push(<RenderFormControlItem key={key} handleData={handleData} dataVal={data[key]} elem={key} />)
+
+        if(key !== "__v" && key !== "_id" && key !== "salt" && key !== "hash" && key !== "albums") {
+            let elem = key;
+            let initialValue = data[key]
+
+            if (elem === "frSent" || elem === "frRecieved" || elem === "friends") {
+                initialValue = data[key].length;
+
+            } else if (elem === "created") {
+                initialValue = moment(data[key]).format("DD-MM-YYYY")
+            }
+            
+            renderData.push(<RenderFormControlItem key={key} handleData={handleData} dataVal={initialValue} elem={key} />)
+        }
     }
 
     return (
-        <Box>
+        <Box sx={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: 11}}>
             <Stack>
-                <ProfilePhoto />
+                {/* <ProfilePhoto /> */}
+                <RenderPhoto ppUrl={data.ppUrl} fullName={data.fullName} />
             </Stack>
-            <Stack>
+            <Stack sx={{width: "45%"}}>
                 {renderData}
             </Stack>
         </Box>
@@ -40,38 +88,58 @@ let RenderFormWithData = ({ handleData, data }) => {
 }
 
 let RenderFormControlItem = ({ handleData, dataVal, elem }) => {
-    let check = ["frSent", "frRcvd", "friends", "created", "email"].includes(elem)
 
-    if (elem === "created") {
-        dataVal = moment(dataVal).format("DD-MM-YYYY")
+    let check = ["frSent", "frRcvd", "frRecieved", "friends", "created", "email", ].includes(elem)
+
+    // if (elem === "frSent" || elem === "frRecieved" || elem === "friends") {
+    //     dataVal = dataVal.length;
+
+    // } else if (elem === "created") {
+    //     dataVal = moment(dataVal).format("DD-MM-YYYY")
+    // }
+
+    let formatElemLabel = () => {
+        let label = ""
+
+        if (elem === "frSent") {
+            label = "Friend Request Sent"
+        } else if (elem === "frRecieved") {
+            label = "Friend Request Recieved"
+        } else {
+            label = elem
+        }
+
+        return label;
     }
 
     return (
-        <FormControl sx={{ m: 2 }} disabled={check}>
-            {elem !== "bio" ? <InputLabel htmlFor={elem}>{elem}</InputLabel> : null}
+        <FormControl sx={{ m: 2 }} disabled={check} value>
+            {/* {elem !== "bio" ? <InputLabel htmlFor={elem}>{elem}</InputLabel> : null} */}
+            {elem !== "bio" ? <InputLabel sx={{textTransform: "capitalize", fontSize: 26, fontWeight: "bold"}} htmlFor={elem}>{formatElemLabel()}</InputLabel> : null}
             {
                 elem === "bio"
                     ?
                     <TextareaAutosize style={{ backgroundColor: "transparent", border: "none", borderBottom: "solid .1px silver" }} minRows={2} maxRows={4} cols={40} defaultValue={dataVal} onChange={e => handleData(e, elem)} />
                     :
-                    <Input type={elem === "email" ? "email" : "text"} defaultValue={dataVal} onChange={e => handleData(e, elem)} />
+                    <Input sx={{fontSize: 29, pl: 2}} type={elem === "email" ? "email" : "text"} defaultValue={dataVal} onChange={e => handleData(e, elem)} />
             }
         </FormControl>
     )
 }
 
-export let CoverPhoto = ({userData}) => {
-    let {cpUrl} = {...userData}
+export let CoverPhoto = ({ userData }) => {
+    let { cpUrl } = { ...userData }
     return (
         <Box sx={{ width: "100%" }}>
-            <ImageListItem cols={1}>
+            <ImageListItem>
                 <img
-                    src={ cpUrl ? cpUrl : `${fakeDataModel[0].coverPhotoUrl}?w500&h299&fit=crop&auto=format`}
+                    src={cpUrl ? cpUrl : `${fakeDataModel[0].coverPhotoUrl}?w500&h299&fit=crop&auto=format`}
                     srcSet={cpUrl ? cpUrl : `${fakeDataModel[0].coverPhotoUrl}?w500&h299&fit=crop&auto=format&dpr= 2 2x`}
                     alt="user X profile cover"
                     loading='lazy'
                 />
                 <ImageListItemBar
+                    sx={{justifyContent: "center"}}
                     // subtitle={"Cover Photo"}
                     title={"Cover Photo"}
                     actionIcon={
@@ -87,16 +155,58 @@ export let CoverPhoto = ({userData}) => {
     )
 }
 
-export let ProfilePhoto = ({ppUrl, fullName}) => {
+export let RenderPhoto = ({ ppUrl, cpUrl, fullName }) => {
+    let decideImgResourceUrl = () => {
+        let src = "";
+
+        if (ppUrl && !cpUrl) {
+            src = `${ppUrl}?w85&h95&fit=crop&auto=format`
+        } else if (cpUrl) {
+            src = `${cpUrl}?w85&h95&fit=crop&auto=format`
+        }
+
+        return src;
+    }
+
     return (
-        <img
-            width={'85px'}
-            height={'95px'}
-            src={`${ ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format`}
-            srcSet={`${ ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format&dpr= 2 2x`}
-            alt={`user ${fullName ? fullName : "X"} profile display`}
-            loading='lazy'
-        />
+        <ImageListItem sx={{width: ppUrl && "560px"}}>
+                <img
+                    // style={{height: ppUrl && "100vh"}}
+                    // src={`${ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format`}
+                    // srcSet={`${ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format&dpr= 2 2x`}
+                    src={decideImgResourceUrl()}
+                    srcSet={`${decideImgResourceUrl()}&dpr= 2 2x`}
+                    alt={`user ${fullName ? fullName : "X"} profile display`}
+                    loading='lazy'
+                    // height={ppUrl && "100vh"}
+                />
+                <ImageListItemBar
+                    sx={{
+                        justifyContent: "center",
+                    }}
+
+                    title={<Typography variant="h6">{ppUrl ? "Profile" : "Cover"} Photo</Typography>}
+
+                    // onClick={toggleShowModal}
+
+                    // actionIcon={
+                    //     <IconButton
+                    //     >
+                    //         <WallpaperRounded
+                    //             sx={{ color: "floralwhite" }}
+                    //         />
+                    //     </IconButton>
+                    // }
+                />
+            </ImageListItem>
+        // <img
+        //     width={'85px'}
+        //     height={'95px'}
+        //     src={`${ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format`}
+        //     srcSet={`${ppUrl ? ppUrl : fakeDataModel[0].coverPhotoUrl}?w85&h95&fit=crop&auto=format&dpr= 2 2x`}
+        //     alt={`user ${fullName ? fullName : "X"} profile display`}
+        //     loading='lazy'
+        // />
     )
 }
 
